5e183c2b42487bd2c00318205f76278048d98ff7
Jesal <jesalchana23@gmail.com>
2022-03-30 20:52:50 +0200

    Added function to check whether all tasks are complete and added sorting to uniqueItem Function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from audioop import reverse
 import csv
 from posixpath import split
 
@@ -28,7 +29,7 @@ def get_unique_user_story(dataList):
     Two dimensional list
 
     Returns:
-    list: list of unique values in the first column of a two-dimensional list 
+    list: list of unique values in the first column of a given two-dimensional list 
     
     """
 
@@ -38,15 +39,62 @@ def get_unique_user_story(dataList):
         itemData = dataList[text][0]
         if itemData not in uniqueValues:
             uniqueValues.append(itemData)
+
+    uniqueValues.sort(key=lambda x: int(x[5:]))
         
     return uniqueValues
+
+def itemCompletionStatus(dataList):
+    """Takes in a 2 dimensional list and returns whether every task for a specific item was completed
+    
+    Parameters: 
+    Two dimensional list
+
+    Returns:
+    list: yes and no response in each element of the list, the response is sorted based on the item number
+    
+    """
+    uniqueItems = get_unique_user_story(dataList)
+    #Ignore heading list
+    dataList.pop(0)
+    ResponseList = []
+    tempList = []
+
+    for x in uniqueItems:
+        for itemIndex in range(0,len(dataList),1):
+            if x == dataList[itemIndex][0]:
+                if dataList[itemIndex][2] == 'Yes':
+                    tempList.append(True)
+                    #print(x + ' ' + dataList[itemIndex][0] + ' ' + dataList[itemIndex][2])
+                elif dataList[itemIndex][2] == 'No':
+                    #print(x + ' ' + dataList[itemIndex][0] + ' ' + dataList[itemIndex][2])
+                    tempList.append(False)
+                else:
+                    tempList.append('BadData')
+        #Check if all values in templist are 'True' then append Yes to ResponseList else return 'No'
+        print(x + ' ' + str(tempList))
+        if all(tempList) == True:
+            ResponseList.append('Yes')
+        else:
+            ResponseList.append('No')
+         #clear all values in the list   
+        tempList.clear()
+        
+    return ResponseList 
+
+    
         
 
 #Main code to invoke functions
 dataset1 = 'dummy.csv'
 dataset2 = 'source_data.csv'
+dataset3 = 'sample-creation-3.csv'
 
-datalist = readFile(dataset1)
+datalist = readFile(dataset3)
 
-print(get_unique_user_story(datalist))
+uniqueItems = get_unique_user_story(datalist)
+
+#print(str(get_unique_user_story(datalist)))
+print(str(itemCompletionStatus(datalist)))
+
 
